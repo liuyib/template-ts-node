79c601fc63690c021f944ccb94894e66e44f6b11
liuyib <1656081615@qq.com>
2020-09-16 23:11:59 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  //   globals: {
+  //     Atomics: 'readonly',
+  //     SharedArrayBuffer: 'readonly',
+  //   },
+  //   parserOptions: {
+  //     ecmaVersion: 2018,
+  //     sourceType: 'module',
+  //   },
+  //   rules: {
+  //     indent: ['error', 2],
+  //     'linebreak-style': ['error', 'unix'],
+  //     quotes: ['error', 'single'],
+  //     semi: ['error', 'always'],
+  //   },
+};
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
