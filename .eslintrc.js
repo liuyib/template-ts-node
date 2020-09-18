3f4aeaab3a4e7105b827403a9b4cfcd3ee9643b6
liuyib <1656081615@qq.com>
2020-09-18 20:15:09 +0800

    chore: remove extra configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,18 +13,4 @@ module.exports = {
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
 };
